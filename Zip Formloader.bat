58352c72130006af02479d9d1f424c83bb3be74b
Andries Smit <mr.andries.smit@gmail.com>
2016-04-14 16:35:51 +0200

    Added Test for Mx 5.21 and Mx6.4
    All seems to be fine... Still waiting for that one day.. we dont need it
    anymore...

--- Zip Formloader.bat
+++ Zip Formloader.bat
@@ -3,6 +3,8 @@ cls
 "C:\Program Files\7-Zip\7z.exe" a -tzip "test\mx56\widgets\FormLoader.mpk" .\src\*
 "C:\Program Files\7-Zip\7z.exe" a -tzip "test\mx515\widgets\FormLoader.mpk" .\src\*
 "C:\Program Files\7-Zip\7z.exe" a -tzip "test\mx517\widgets\FormLoader.mpk" .\src\*
+"C:\Program Files\7-Zip\7z.exe" a -tzip "test\mx521\widgets\FormLoader.mpk" .\src\*
+"C:\Program Files\7-Zip\7z.exe" a -tzip "test\mx6.4\widgets\FormLoader.mpk" .\src\*
 TIMEOUT /T 3
 goto start
 
